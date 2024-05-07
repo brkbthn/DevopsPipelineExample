7a0cb2036b143a69382e41da547f1f5aa6e89f1d
brkbthn <berk.batuhan595@gmail.com>
2024-05-07 14:35:34 +0300

    first commit

---
+++ src/main/java/com/batuhandevran/JavaSpringBootPorjectApplication.java
@@ -0,0 +1,13 @@
+package com.batuhandevran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootPorjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootPorjectApplication.class, args);
+    }
+
+}
